fc7b8592a0d2a2ac65164268690f1e58c07715ad
Sarah Ng <sarahng@deloitte.com>
2018-07-19 12:56:51 -0500

    still trying to disable regex checks..

--- checks.go
+++ checks.go
@@ -51,34 +51,29 @@ func doChecks(diff string, commit Commit, repo *Repo) []Leak {
 		}
 
 		// Check for external regex matches
-		if externalRegex != nil {
-			for _, re := range externalRegex {
-				match = re.FindString(line)
-				// if len(match) == 0 ||
-				// 	(opts.Strict && containsStopWords(line)) ||
-				// 	(opts.Entropy && !checkShannonEntropy(line, opts)) {
-				// 	continue
-				// }
+		// if externalRegex != nil {
+		// 	for _, re := range externalRegex {
+		// 		match = re.FindString(line)
+		// 		if len(match) == 0 ||
+		// 			(opts.Strict && containsStopWords(line)) ||
+		// 			(opts.Entropy && !checkShannonEntropy(line, opts)) {
+		// 			continue
+		// 		}
 
-				if len(match) == 0 ||
-					(opts.Entropy && !checkShannonEntropy(line, opts)) {
-					continue
-				}
-
-				leak = Leak{
-					Line:     line,
-					Commit:   commit.Hash,
-					Offender: match,
-					Reason:   "match: " + re.String(),
-					Msg:      commit.Msg,
-					Time:     commit.Time,
-					Author:   commit.Author,
-					File:     file,
-					RepoURL:  repo.url,
-				}
-				leaks = append(leaks, leak)
-			}
-		}
+		// 		leak = Leak{
+		// 			Line:     line,
+		// 			Commit:   commit.Hash,
+		// 			Offender: match,
+		// 			Reason:   "match: " + re.String(),
+		// 			Msg:      commit.Msg,
+		// 			Time:     commit.Time,
+		// 			Author:   commit.Author,
+		// 			File:     file,
+		// 			RepoURL:  repo.url,
+		// 		}
+		// 		leaks = append(leaks, leak)
+		// 	}
+		// }
 	}
 	return leaks
 
